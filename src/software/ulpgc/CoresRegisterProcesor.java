106e1349aa51d02f31ee6882cef57cc490f856ab
Yeremay <1yeremay1@gmail.com>
2023-12-07 01:40:23 +0000

    class CoresRegisterProcesor implemented

---
+++ src/software/ulpgc/CoresRegisterProcesor.java
@@ -0,0 +1,18 @@
+package software.ulpgc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CoresRegisterProcesor implements RegisterProcesor{
+    @Override
+    public Map<Integer, Integer> process(List<Register> registers) {
+        Map<Integer, Integer> result = new HashMap<>();
+        for (Register register: registers){
+            int Cores = register.getCores();
+            if(Cores == -1) continue;
+            result.put(Cores,result.getOrDefault(Cores,0)+1);
+        }
+        return result;
+    }
+}

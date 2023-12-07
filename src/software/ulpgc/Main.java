6e2aedf322079d9a806b7ac6c5da447c021042a6
Yeremay <1yeremay1@gmail.com>
2023-12-07 01:43:25 +0000

    Final State

--- src/software/ulpgc/Main.java
+++ src/software/ulpgc/Main.java
@@ -1,7 +1,16 @@
 package software.ulpgc;
 
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
+        RegisterLoader loader = new CSVFileRegisterLoader(new File("CPU_benchmark_v4.csv"));
+        List<Register> registers = loader.load();
+        Map<Integer, Integer> ColumnChart = new CoresRegisterProcesor().process(registers);
+        for (int key: ColumnChart.keySet().stream().sorted().toList()){
+            System.out.println(key + " : " + ColumnChart.get(key));
+        }
     }
 }
